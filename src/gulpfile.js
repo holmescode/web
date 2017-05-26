7970dab92bd9120299ae5ecf56dcfdea4db6a95f
Matt Holmes <pdxmholmes@users.noreply.github.com>
2017-05-25 20:34:16 -0700

    Cleaned up build structure to just use gulp for resources Fixed some CSS issues

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,11 +1,30 @@
-var gulp = require("gulp"),
-    fs = require("fs"),
-    sass = require("gulp-sass");
+var gulp = require('gulp'),
+    fs = require('fs'),
+    sass = require('gulp-sass'),
+    cssmin = require('gulp-cssmin'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify');
 
 // other content removed
 
-gulp.task("default", function () {
+gulp.task('sass', function () {
     return gulp.src('Styles/main.scss')
         .pipe(sass())
         .pipe(gulp.dest('wwwroot/css'));
 });
+
+gulp.task('sass:min', ['sass'], function () {
+    return gulp.src('wwwroot/css/main.css')
+        .pipe(cssmin())
+        .pipe(rename('main.min.css'))
+        .pipe(gulp.dest('wwwroot/css'));
+});
+
+gulp.task('js:min', function () {
+    return gulp.src('wwwroot/js/site.js')
+        .pipe(uglify())
+        .pipe(rename('site.min.js'))
+        .pipe(gulp.dest('wwwroot/js'));
+});
+
+gulp.task('default', ['sass', 'sass:min', 'js:min']);
